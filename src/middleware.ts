25614083a422636dde16716eb669fa69961273f8
Shubham <sh007shubham@gmail.com>
2026-05-24 16:38:28 +0530

    fix: add 'unsafe-inline' to CSP script-src for Next.js hydration

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,7 +21,7 @@ function applySecurityHeaders(res: NextResponse): NextResponse {
     "Content-Security-Policy",
     [
       "default-src 'self'",
-      "script-src 'self'",
+      "script-src 'self' 'unsafe-inline'",
       "style-src 'self' 'unsafe-inline'",
       "img-src 'self' data: blob: https:",
       "font-src 'self' data:",
